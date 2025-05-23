3511f3de1ce4e398da9fd957a9997a7a7e6d9108
Kleber Garcia Alcatrao <kleber@ziontechgroup.com>
2025-05-23 03:39:25 -0300

    Specify return type for useAuth

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,10 +1,11 @@
 import { useContext } from 'react';
 import { AuthContext } from '@/context/auth/AuthContext';
+import type { AuthContextType } from '@/types/auth';
 
-export const useAuth = () => {
+export const useAuth = (): AuthContextType => {
   const context = useContext(AuthContext);
   if (context === undefined) {
     throw new Error('useAuth must be used within an AuthProvider');
   }
-  return context;
+  return context as AuthContextType;
 };
