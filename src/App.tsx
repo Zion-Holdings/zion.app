1e9ea0708426df59b261b14925eb7b9f365449c7
Kleber Garcia Alcatrao <kleber@ziontechgroup.com>
2025-05-23 10:59:02 -0300

    Fix React Suspense import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React, { Suspense } from 'react';
+import React from 'react';
+import Suspense from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import { ThemeProvider } from "./components/ThemeProvider";
