ffdf219ecd9c870b0677dc5491eff7bc52232f39
Kleber Garcia Alcatrao <kleber@ziontechgroup.com>
2025-05-23 16:11:06 -0300

    Fix proxy conditional to place then on new line

--- setup.sh
+++ setup.sh
@@ -4,7 +4,11 @@
 set -e
 
 # Normalize proxy environment variables to avoid npm warnings
-if [ -n "$npm_config_http_proxy" ]; then
+# Some restricted shells do not fully support the '[' alias used in Bash
+# conditionals. Use 'test' directly for maximum portability. Place 'then' on a
+# separate line to avoid syntax errors in limited shells.
+if test -n "$npm_config_http_proxy"
+then
   export npm_config_proxy="$npm_config_http_proxy"
   unset npm_config_http_proxy
 fi
