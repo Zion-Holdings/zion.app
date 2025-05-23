98e92735d84fbfe09314a350b7b97263f6d6a45a
Kleber Garcia Alcatrao <kleber@ziontechgroup.com>
2025-05-22 22:13:53 -0300

    Allow toast description field

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -129,9 +129,10 @@ export {
 // Add useToast hook export
 export function useToast() {
   return {
-    toast: (props: ToastProps) => {
+    // Accept a loosely typed props object to allow custom fields like `description`
+    toast: (props: any) => {
       // Implementation of toast functionality
-      console.log('Toast:', props)
-    }
+      console.log("Toast:", props)
+    },
   }
 }
