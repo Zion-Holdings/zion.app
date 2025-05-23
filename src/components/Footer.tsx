36a4b3ff9e8e398dc2990a2bbe045cc1de6ba479
Kleber Garcia Alcatrao <kleber@ziontechgroup.com>
2025-05-23 10:47:10 -0300

    Fix careers link in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -51,7 +51,7 @@ export function Footer() {
               <li><Link to="/content/about" className="text-zion-slate hover:text-zion-cyan transition-colors">About Us</Link></li>
               <li><Link to="/blog" className="text-zion-slate hover:text-zion-cyan transition-colors">Blog</Link></li>
               <li><Link to="/partners" className="text-zion-slate hover:text-zion-cyan transition-colors">Partners</Link></li>
-              <li><Link to="/careers" className="text-zion-slate hover:text-zion-cyan transition-colors">Careers</Link></li>
+              <li><Link to="/content/careers" className="text-zion-slate hover:text-zion-cyan transition-colors">Careers</Link></li>
               <li><Link to="/contact" className="text-zion-slate hover:text-zion-cyan transition-colors">Contact</Link></li>
               <li><Link to="/sitemap-page" className="text-zion-slate hover:text-zion-cyan transition-colors">Sitemap</Link></li>
             </ul>
