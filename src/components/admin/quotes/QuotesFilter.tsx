78caf8293e921ede32f9ac11ee7bb6153f8b47c5
Kleber Garcia Alcatrao <kleber@ziontechgroup.com>
2025-05-23 05:36:49 -0300

    fix types

--- src/components/admin/quotes/QuotesFilter.tsx
+++ src/components/admin/quotes/QuotesFilter.tsx
@@ -7,7 +7,7 @@ import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover
 import { Button } from "@/components/ui/button";
 import { Calendar as CalendarComponent } from "@/components/ui/calendar";
 import { format } from "date-fns";
-import type { DateRange } from "react-day-picker";
+import type { DateRange } from "@/types/dateRange";
 import type { QuoteStatus } from "@/types/quotes";
 
 interface QuotesFilterProps {
