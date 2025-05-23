e0b9759727fe85f163f4b766a49cde1b7ecd0e6a
Kleber Garcia Alcatrao <kleber@ziontechgroup.com>
2025-05-23 06:32:06 -0300

    Add WhitelabelProvider to App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import { ThemeProvider } from "./components/ThemeProvider";
+import { WhitelabelProvider } from "./context/WhitelabelContext";
 import { Toaster } from "./components/ui/toaster";
 import { Toaster as SonnerToaster } from "./components/ui/sonner";
 import {
@@ -21,24 +22,26 @@ import Home from './pages/Home';
 
 const App = () => {
   return (
-    <ThemeProvider defaultTheme="dark">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/auth/*" element={<AuthRoutes />} />
-        <Route path="/dashboard/*" element={<DashboardRoutes />} />
-        <Route path="/marketplace/*" element={<MarketplaceRoutes />} />
-        <Route path="/talent/*" element={<TalentRoutes />} />
-        <Route path="/admin/*" element={<AdminRoutes />} />
-        <Route path="/mobile/*" element={<MobileAppRoutes />} />
-        <Route path="/content/*" element={<ContentRoutes />} />
-        <Route path="/enterprise/*" element={<EnterpriseRoutes />} />
-        <Route path="/community/*" element={<CommunityRoutes />} />
-        <Route path="/developers/*" element={<DeveloperRoutes />} />
-        <Route path="*" element={<ErrorRoutes />} />
-      </Routes>
-      <Toaster />
-      <SonnerToaster position="top-right" />
-    </ThemeProvider>
+    <WhitelabelProvider>
+      <ThemeProvider defaultTheme="dark">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth/*" element={<AuthRoutes />} />
+          <Route path="/dashboard/*" element={<DashboardRoutes />} />
+          <Route path="/marketplace/*" element={<MarketplaceRoutes />} />
+          <Route path="/talent/*" element={<TalentRoutes />} />
+          <Route path="/admin/*" element={<AdminRoutes />} />
+          <Route path="/mobile/*" element={<MobileAppRoutes />} />
+          <Route path="/content/*" element={<ContentRoutes />} />
+          <Route path="/enterprise/*" element={<EnterpriseRoutes />} />
+          <Route path="/community/*" element={<CommunityRoutes />} />
+          <Route path="/developers/*" element={<DeveloperRoutes />} />
+          <Route path="*" element={<ErrorRoutes />} />
+        </Routes>
+        <Toaster />
+        <SonnerToaster position="top-right" />
+      </ThemeProvider>
+    </WhitelabelProvider>
   );
 };
 
