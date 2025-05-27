60bb9e311485e308abd4ac2de6cbba2460ddb9b0
Kleber Garcia Alcatrao <kleber@ziontechgroup.com>
2025-05-27 10:37:32 -0300

    fix jest config for esm

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,13 @@
-module.exports = {
-  preset: 'ts-jest',
+export default {
+  preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'jsdom',
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
   setupFilesAfterEnv: ['<rootDir>/tests/jest.setup.ts'],
   moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
     '^@/(.*)$': '<rootDir>/src/$1',
   },
+  transform: {},
   roots: ['<rootDir>/tests'],
   coverageThreshold: {
     global: {
