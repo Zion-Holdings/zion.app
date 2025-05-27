eec8b7a5c7fe801839f965e3a82e7ae494dee748
Kleber Garcia Alcatrao <kleber@ziontechgroup.com>
2025-05-27 10:34:22 -0300

    chore(jest): enable esm support

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,13 @@
 module.exports = {
-  preset: 'ts-jest',
+  preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/tests/jest.setup.ts'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
   roots: ['<rootDir>/tests'],
+  coverageProvider: 'v8',
   coverageThreshold: {
     global: {
       lines: 80,
