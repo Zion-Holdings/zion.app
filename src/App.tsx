d31b246d0ecb4ea6e72685e01429814144d94055
Kleber Garcia Alcatrao <kleber@ziontechgroup.com>
2025-05-29 17:13:23 +0000

    Add new services page and data

--- src/App.tsx
+++ src/App.tsx
@@ -36,6 +36,7 @@ import Categories from './pages/Categories';
 import Blog from './pages/Blog';
 import BlogPost from './pages/BlogPost';
 import NewProductsPage from './pages/NewProductsPage';
+import NewServicesPage from './pages/NewServicesPage';
 import Sitemap from './pages/Sitemap';
 import PartnersPage from './pages/Partners';
 import Login from './pages/Login';
@@ -61,6 +62,7 @@ const baseRoutes = [
   { path: '/equipment', element: <EquipmentPage /> },
   { path: '/equipment/:id', element: <EquipmentDetail /> },
   { path: '/new-products', element: <NewProductsPage /> },
+  { path: '/new-services', element: <NewServicesPage /> },
   { path: '/analytics', element: <Analytics /> },
   { path: '/mobile-launch', element: <MobileLaunchPage /> },
   { path: '/open-app', element: <OpenAppRedirect /> },
