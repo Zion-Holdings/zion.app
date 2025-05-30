2b0fc3223776a579dd0c51fe2785e7f717dea819
Kleber Garcia Alcatrao <kleber@ziontechgroup.com>
2025-05-30 03:14:50 +0000

    Fix jest config and imports

--- jest.config.js
+++ jest.config.js
@@ -1,23 +1,16 @@
 export default {
   preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'jsdom',
-  extensionsToTreatAsEsm: ['.ts'],
-  globals: {
-    'ts-jest': {
-      useESM: true,
-    },
-  },
-  setupFilesAfterEnv: ['<rootDir>/tests/jest.setup.ts'],
   extensionsToTreatAsEsm: ['.ts', '.tsx'],
   globals: {
     'ts-jest': {
       useESM: true,
     },
   },
+  setupFilesAfterEnv: ['<rootDir>/tests/jest.setup.ts'],
   moduleNameMapper: {
     '^@/pages/(.*)$': '<rootDir>/src/pages/$1',
     '^@/(.*)$': '<rootDir>/src/$1',
-    '^@/pages/(.*)$': '<rootDir>/src/pages/$1',
     '^vitest$': '<rootDir>/tests/vitest-mock.ts',
   },
   roots: ['<rootDir>/__tests__', '<rootDir>/tests'],
