4b102db56ef49e3c59899424de1c0787b82492c5
Kleber Garcia Alcatrao <kleber@ziontechgroup.com>
2025-05-30 00:01:07 +0000

    Remove unsafe-eval from CSP and disable stripe cookies

--- src/utils/getStripe.ts
+++ src/utils/getStripe.ts
@@ -8,7 +8,7 @@ export function getStripe() {
       process.env.NODE_ENV === 'production'
         ? (import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY as string)
         : (import.meta.env.NEXT_PUBLIC_STRIPE_TEST_KEY as string);
-    stripePromise = loadStripe(key);
+    stripePromise = loadStripe(key, { advancedFraudSignals: false });
   }
   return stripePromise;
 }
