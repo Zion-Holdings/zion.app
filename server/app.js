7ebe85f4b8c467d7e24b79e731b0d0061d59182a
Kleber Garcia Alcatrao <kleber@ziontechgroup.com>
2025-06-01 04:21:12 +0000

    Add recommendations stub and beta indicator

--- server/app.js
+++ server/app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const { mongoUri } = require('./config');
 const authRoutes = require('./routes/auth');
+const recommendationsRoutes = require('./routes/recommendations');
 
 const app = express();
 app.use(express.json());
 app.use('/auth', authRoutes);
+app.use('/recommendations', recommendationsRoutes);
 
 mongoose.connect(mongoUri, {
   useNewUrlParser: true,
