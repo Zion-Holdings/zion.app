b5ed57bca86a16d3829c74001ef6348349edf946
Kleber Garcia Alcatrao <kleber@ziontechgroup.com>
2025-06-02 17:41:31 +0000

    fix: remove obsolete UserMenu export

--- src/components/header/index.ts
+++ src/components/header/index.ts
@@ -2,7 +2,6 @@
 export { Header } from './Header';
 export { Logo } from './Logo';
 export { MobileMenu, type MobileMenuProps } from './MobileMenu';
-export { UserMenu } from './UserMenu';
 export { UserProfileDropdown } from './UserProfileDropdown';
 export { AvatarMenu } from './AvatarMenu';
 export { MainNavigation } from './MainNavigation';
