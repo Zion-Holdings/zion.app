c8d10cfcf700767826135799c9b1a93c20021cc3
Kleber Garcia Alcatrao <kleber@ziontechgroup.com>
2025-06-02 17:41:53 +0000

    fix header re-export

--- src/components/header/index.ts
+++ src/components/header/index.ts
@@ -3,6 +3,6 @@ export { Header } from './Header';
 export { Logo } from './Logo';
 export { MobileMenu, type MobileMenuProps } from './MobileMenu';
 export { UserMenu } from './UserMenu';
-export { UserProfileDropdown } from './UserProfileDropdown';
+export { default as UserProfileDropdown } from './UserProfileDropdown';
 export { AvatarMenu } from './AvatarMenu';
 export { MainNavigation } from './MainNavigation';
