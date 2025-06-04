1bb420fbcda23bc7fd5ba1c371ed4ce3727b23ef
Kleber Garcia Alcatrao <kleber@ziontechgroup.com>
2025-06-04 10:40:02 +0000

    Add stub type definitions for Next and other modules

---
+++ src/types/react-router-dom.d.ts
@@ -0,0 +1,12 @@
+declare module 'react-router-dom' {
+  const ReactRouterDom: any;
+  export default ReactRouterDom;
+  export const BrowserRouter: any;
+  export const Routes: any;
+  export const Route: any;
+  export const Link: any;
+  export function useNavigate(): any;
+  export function useLocation(): any;
+  export function useParams(): any;
+  export function useSearchParams(): any;
+}
