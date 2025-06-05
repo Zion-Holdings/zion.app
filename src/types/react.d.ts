12980dd449f6fa1bebe1434b1790dd122bdee355
Kleber Garcia Alcatrao <kleber@ziontechgroup.com>
2025-06-05 21:52:31 +0000

    chore(types): add react stub

---
+++ src/types/react.d.ts
@@ -0,0 +1,15 @@
+declare module 'react' {
+  const React: any;
+  export default React;
+  export const useState: any;
+  export const useEffect: any;
+  export const useContext: any;
+  export const createContext: any;
+  export const useRef: any;
+  export const useMemo: any;
+  export const useCallback: any;
+  export const forwardRef: any;
+  export const Fragment: any;
+  export const Children: any;
+  export const cloneElement: any;
+}
