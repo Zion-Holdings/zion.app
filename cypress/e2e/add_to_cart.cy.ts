33ff1848a9a4cd78fa12fab93d79ca3f75a44cbc
Kleber Garcia Alcatrao <kleber@ziontechgroup.com>
2025-06-06 02:40:56 +0000

    Implement redux cart with badge and tests

--- cypress/e2e/add_to_cart.cy.ts
+++ cypress/e2e/add_to_cart.cy.ts
@@ -2,7 +2,7 @@ describe('add to cart', () => {
   it('stores item in local storage', () => {
     cy.visit('/equipment/pro-camera-x1000');
     cy.contains('Add to Cart').click();
-    cy.wait(900);
+    cy.url().should('include', '/cart');
     cy.window().then((win) => {
       const cart = JSON.parse(win.localStorage.getItem('cart') || '[]');
       expect(cart.length).to.be.greaterThan(0);
