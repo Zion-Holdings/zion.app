8966fc8f179d3a76b02dfd18d838a9fc1dc95c86
Kleber Garcia Alcatrao <kleber@ziontechgroup.com>
2025-06-08 22:53:46 +0000

    Wrap Next.js app with router

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
 import { HelmetProvider } from 'react-helmet-async';
-import { AuthProvider } from '@/context/auth/AuthProvider'; // Assuming @ is src/
+import { AuthProvider } from '@/context/auth/AuthProvider';
 import { Provider as ReduxProvider } from 'react-redux';
-import { store } from '@/store'; // Changed to named import
-import { WhitelabelProvider } from '@/context/WhitelabelContext'; // Added WhitelabelProvider
+import { store } from '@/store';
+import { WhitelabelProvider } from '@/context/WhitelabelContext';
+import { BrowserRouter, MemoryRouter } from 'react-router-dom';
 // If you have global CSS, import it here:
 // import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const isBrowser = typeof window !== 'undefined';
+  const Router = isBrowser ? BrowserRouter : MemoryRouter;
   return (
     <ReduxProvider store={store}>
       <HelmetProvider>
         <AuthProvider>
           <WhitelabelProvider>
-            <Component {...pageProps} />
+            <Router>
+              <Component {...pageProps} />
+            </Router>
           </WhitelabelProvider>
         </AuthProvider>
       </HelmetProvider>
