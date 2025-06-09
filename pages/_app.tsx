7c4182b9e464a2cd937e73d018af8ad444dfbfd4
Kleber Garcia Alcatrao <kleber@ziontechgroup.com>
2025-06-09 00:52:38 +0000

    Fix _app.tsx merge issue

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,6 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter, MemoryRouter } from 'react-router-dom';
 import type { AppProps } from 'next/app';
 import { StaticRouter } from 'react-router-dom/server'; // Changed from BrowserRouter
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { HelmetProvider } from 'react-helmet-async';
 import { AuthProvider } from '@/context/auth/AuthProvider';
 import { Provider as ReduxProvider } from 'react-redux';
@@ -12,13 +11,6 @@ import { WhitelabelProvider } from '@/context/WhitelabelContext'; // Added White
 import { WalletProvider } from '@/context/WalletContext'; // Added WalletProvider
 import { AnalyticsProvider } from '@/context/AnalyticsContext'; // Added AnalyticsProvider
 import { CartProvider } from '@/context/CartContext'; // Added CartProvider
-// If you have global CSS, import it here:
-// import '../styles/globals.css';
-
-// Create a client
-const queryClient = new QueryClient();
-
-function MyApp({ Component, pageProps, router }: AppProps) { // Added router to props
 import { I18nextProvider } from 'react-i18next';
 import i18n from '@/i18n';
 import { Toaster } from '@/components/ui/toaster';
