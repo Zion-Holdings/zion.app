c319632e27c1cbf14e97b71a6f3c35438a611ef6
Kleber Garcia Alcatrao <kleber@ziontechgroup.com>
2025-06-09 21:51:37 +0000

    Add maintenance automation

--- server/app.js
+++ server/app.js
@@ -9,12 +9,19 @@ const authSocialRoutes = require('./routes/authSocial');
 const recommendationsRoutes = require('./routes/recommendations');
 const syncRoutes = require('./routes/sync');
 const { logAndAlert } = require('./utils/alertLogger');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const cors = require('cors');
 
 const app = express();
 app.use(morgan('dev'));
 app.use(mongooseMorgan({ connectionString: mongoUri }));
 app.use(express.json());
 app.use(passport.initialize());
+app.use(helmet());
+app.use(cors());
+const limiter = rateLimit({ windowMs: 15 * 60 * 1000, max: 100 });
+app.use(limiter);
 
 app.use('/auth', authRoutes);
 // Provide /api/auth routes for frontend API consistency
@@ -22,6 +29,9 @@ app.use('/api/auth', authRoutes);
 app.use('/', authSocialRoutes);
 app.use('/recommendations', recommendationsRoutes);
 app.use('/sync', syncRoutes);
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'ok' });
+});
 
 mongoose.connect(mongoUri, {
   useNewUrlParser: true,
@@ -32,9 +42,7 @@ mongoose.connect(mongoUri, {
 app.use((err, req, res, next) => {
   console.error(err);
   logAndAlert(err.stack || err.message);
-  res
-    .status(err.status || 500)
-    .json({ code: err.code, message: err.message });
+  res.status(err.status || 500).json({ code: err.code, message: err.message });
 });
 
 module.exports = app;
