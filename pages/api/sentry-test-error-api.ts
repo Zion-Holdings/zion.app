7c6c367bfad61beeb8637055c227921a117f9c52
Kleber Garcia Alcatrao <kleber@ziontechgroup.com>
2025-06-10 16:41:58 +0000

    Capture Sentry errors in API route

--- pages/api/sentry-test-error-api.ts
+++ pages/api/sentry-test-error-api.ts
@@ -8,11 +8,13 @@ export default async function handler(
 ) {
   try {
     // This will intentionally throw an error
-    throw new Error('Sentry Test API Error: This is a test error from the API route.');
+    throw new Error('Sentry Test API Error');
   } catch (error) {
     // Capture the error with Sentry
     Sentry.captureException(error);
     // Respond with an error status
-    res.status(500).json({ error: 'Sentry Test API Error: An error was thrown and captured by Sentry.' });
+    res.status(500).json({ error: 'Sentry Test API Error' });
+    // Ensure the event is sent before the function exits (important for Netlify)
+    await Sentry.flush(2000);
   }
 }
--- sentry.server.config.js
+++ sentry.server.config.js
@@ -2,11 +2,13 @@
 import * as Sentry from "@sentry/nextjs";
 
 const SENTRY_DSN = process.env.SENTRY_DSN;
+const SENTRY_LOG_LEVEL = process.env.SENTRY_LOG_LEVEL;
 
 Sentry.init({
   dsn: SENTRY_DSN,
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
+  debug: SENTRY_LOG_LEVEL === 'debug',
   // ...
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
