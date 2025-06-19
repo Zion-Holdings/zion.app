673998baad9c036d089df7ebdc65f982365cb397
Kleber Garcia Alcatrao <kleber@ziontechgroup.com>
2025-06-19 23:59:39 +0000

    168

--- src/utils/sentry.ts
+++ src/utils/sentry.ts
@@ -21,14 +21,6 @@ export function captureException(error: unknown, context?: any) { // Added conte
   } else {
     Sentry.captureException(error);
   }
-  // Optionally, add a development console log if you still want to see errors when not connected to Sentry,
-  // though Sentry's SDK might also provide some console output in dev mode by default.
-  if (
-    process.env.NODE_ENV === 'development' &&
-    (!process.env.NEXT_PUBLIC_SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN.startsWith('YOUR_'))
-  ) {
-    console.error('Sentry DSN not configured, logging error to console:', error, context);
-  }
 }
 
 // It's good practice to also export Sentry itself if you need to use other Sentry methods elsewhere.
