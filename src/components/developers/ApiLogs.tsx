4765515bafc712d4c15d0f66073c4cdc8e51e431
Kleber Garcia Alcatrao <kleber@ziontechgroup.com>
2025-06-20 00:22:00 -0300

    Update ApiLogs.tsx
    247

--- src/components/developers/ApiLogs.tsx
+++ src/components/developers/ApiLogs.tsx
@@ -5,7 +5,7 @@ import { List, RefreshCw } from "lucide-react";
 import { useApiKeys } from "@/hooks/useApiKeys";
 
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Badge } from "@/components/ui/badge";
 import { ApiLogsChart } from "./ApiLogsChart";
