3efafebd4d35cdf0c02f343f280aa2b842dab8d0
Kleber Garcia Alcatrao <kleber@ziontechgroup.com>
2025-06-20 11:30:37 +0000

    264

--- src/routes/AdminRoutes.tsx
+++ src/routes/AdminRoutes.tsx
@@ -20,145 +20,133 @@ import ZionBrain from "@/pages/admin/ZionBrain";
 import MetricsDashboard from "@/pages/dashboard/MetricsDashboard";
 import FounderBackupVault from "@/pages/FounderBackupVault";
 
-const AdminRoutes = () => {
-  return (
-    <Routes>
-      <Route
-        path="/admin/quotes"
-        element={
-          <ProtectedRoute adminOnly={true}>
-            <QuoteManager />
-          </ProtectedRoute>
-        }
-      />
+// const AdminRoutes = () => {
+//   return (
+//     <Routes>
+//       <Route
+//         path="/admin/quotes"
+//         element={
+//           <ProtectedRoute adminOnly={true}>
+//             <QuoteManager />
+//           </ProtectedRoute>
+//         }
+//       />
       
-      <Route
-        path="/admin/reviews"
-        element={
-          <ProtectedRoute adminOnly={true}>
-            <ReviewsModeration />
-          </ProtectedRoute>
-        }
-      />
+//       <Route
+//         path="/admin/reviews"
+//         element={
+//           <ProtectedRoute adminOnly={true}>
+//             <ReviewsModeration />
+//           </ProtectedRoute>
+//         }
+//       />
       
-      <Route
-        path="/admin/whitelabel"
-        element={
-          <ProtectedRoute adminOnly={true}>
-            <WhitelabelManager />
-          </ProtectedRoute>
-        }
-      />
+//       <Route
+//         path="/admin/whitelabel"
+//         element={
+//           <ProtectedRoute adminOnly={true}>
+//             <WhitelabelManager />
+//           </ProtectedRoute>
+//         }
       
-      <Route
-        path="/admin/partners"
-        element={
-          <ProtectedRoute adminOnly={true}>
-            <PartnerManager />
-          </ProtectedRoute>
-        }
-      />
-      
-      <Route
-        path="/admin/app-metadata"
-        element={
-          <ProtectedRoute adminOnly={true}>
-            <AppMetadataManager />
-          </ProtectedRoute>
-        }
-      />
-      
-      <Route
-        path="/admin/support-requests"
-        element={
-          <ProtectedRoute adminOnly={true}>
-            <SupportRequests />
-          </ProtectedRoute>
-        }
-      />
-      
-      <Route
-        path="/admin/fraud-detection"
-        element={
-          <ProtectedRoute adminOnly={true}>
-            <FraudDetection />
-          </ProtectedRoute>
-        }
-      />
 
-      <Route
-        path="/admin/moderation"
-        element={
-          <ProtectedRoute adminOnly={true}>
-            <ListingModeration />
-          </ProtectedRoute>
-        }
-      />
+//       <Route
+//         path="/admin/partners"
+//         element={
+//           <ProtectedRoute adminOnly={true}>
+//             <PartnerManager />
+//           </ProtectedRoute>
+//         }
 
-      <Route
-        path="/admin/tokens"
-        element={
-          <ProtectedRoute adminOnly={true}>
-            <TokenManager />
-          </ProtectedRoute>
-        }
-      />
-      
-      <Route
-        path="/admin/integrations"
-        element={
-          <ProtectedRoute adminOnly={true}>
-            <IntegrationsPage />
-          </ProtectedRoute>
-        }
-      />
+//       <Route
+//         path="/admin/app-metadata"
+//         element={
+//           <ProtectedRoute adminOnly={true}>
+//             <AppMetadataManager />
+//           </ProtectedRoute>
+//         }
 
-      <Route
-        path="/admin/kyc"
-        element={
-          <ProtectedRoute adminOnly={true}>
-            <KycDashboard />
-          </ProtectedRoute>
-        }
-      />
+//       <Route
+//         path="/admin/support-requests"
+//         element={
+//           <ProtectedRoute adminOnly={true}>
+//             <SupportRequests />
+//           </ProtectedRoute>
+//         }
 
-      <Route
-        path="/admin/whitepaper-generator"
-        element={
-          <ProtectedRoute adminOnly={true}>
-            <WhitepaperGeneratorPage />
-          </ProtectedRoute>
-        }
-      />
+//       <Route
+//         path="/admin/fraud-detection"
+//         element={
+//           <ProtectedRoute adminOnly={true}>
+//             <FraudDetection />
+//           </ProtectedRoute>
+//         }
 
-      <Route
-        path="/admin/zion-brain"
-        element={
-          <ProtectedRoute adminOnly={true}>
-            <ZionBrain />
-          </ProtectedRoute>
-        }
-      />
+//       <Route
+//         path="/admin/moderation"
+//         element={
+//           <ProtectedRoute adminOnly={true}>
+//             <ListingModeration />
+//           </ProtectedRoute>
+//         }
 
-      <Route
-        path="/admin/vault"
-        element={
-          <ProtectedRoute adminOnly={true}>
-            <FounderBackupVault />
-          </ProtectedRoute>
-        }
-      />
+//       <Route
+//         path="/admin/tokens"
+//         element={
+//           <ProtectedRoute adminOnly={true}>
+//             <TokenManager />
+//           </ProtectedRoute>
+//         }
 
-      <Route
-        path="/admin/kpi"
-        element={
-          <ProtectedRoute adminOnly={true}>
-            <MetricsDashboard adminView />
-          </ProtectedRoute>
+//       <Route
+//         path="/admin/integrations"
+//         element={
+//           <ProtectedRoute adminOnly={true}>
+//             <IntegrationsPage />
+//           </ProtectedRoute>
+//         }
+
+//       <Route
+//         path="/admin/kyc"
+//         element={
+//           <ProtectedRoute adminOnly={true}>
+//             <KycDashboard />
+//           </ProtectedRoute>
+//         }
+
+//       <Route
+//         path="/admin/whitepaper-generator"
+//         element={
+//           <ProtectedRoute adminOnly={true}>
+//             <WhitepaperGeneratorPage />
+//           </ProtectedRoute>
+//         }
+
+//       <Route
+//         path="/admin/zion-brain"
+//         element={
+//           <ProtectedRoute adminOnly={true}>
+//             <ZionBrain />
+//           </ProtectedRoute>
+        }
+
+//       <Route
+//         path="/admin/vault"
+//         element={
+//           <ProtectedRoute adminOnly={true}>
+//             <FounderBackupVault />
+//           </ProtectedRoute>
+        }
+//       <Route
+//         path="/admin/kpi"
+//         element={
+//           <ProtectedRoute adminOnly={true}>
+//             <MetricsDashboard adminView />
+//           </ProtectedRoute>
         }
       />
-    </Routes>
-  );
-};
+//     </Routes>
+//   );
+// };
 
 export default AdminRoutes;
