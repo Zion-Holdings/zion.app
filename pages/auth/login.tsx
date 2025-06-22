0b6662b9788618cd15f568104cd9e277974f6f7c
Kleber Garcia Alcatrao <kleber@ziontechgroup.com>
2025-06-22 09:47:30 -0300

    Update login.tsx
    360

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,4 +1,4 @@
-import { useUser } from '@auth0/nextjs-auth0/client';
+import { useUser } from '@auth0/nextjs-auth0';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import Link from 'next/link';
