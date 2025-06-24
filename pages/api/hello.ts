d778aa5cdb48eea171501bed810e9a9ed2118f14
Kleber Garcia Alcatrao <kleber@ziontechgroup.com>
2025-06-24 11:47:11 -0300

    376
    376

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -6,5 +6,5 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
     return res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 
-  res.status(200).json({ message: 'Hello world' });
+  return res.status(200).json({ message: 'Hello world' });
 }
