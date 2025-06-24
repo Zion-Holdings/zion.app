ba4450cdba77f7199fc1c932c795c436378fe636
Kleber Garcia Alcatrao <kleber@ziontechgroup.com>
2025-06-24 11:48:35 -0300

    379
    379

--- pages/api/push/send.ts
+++ pages/api/push/send.ts
@@ -30,5 +30,5 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       })
     )
   );
-  res.status(200).json({ success: true });
+      return res.status(200).json({ success: true });
 }
