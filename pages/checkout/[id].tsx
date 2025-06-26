8e332d3ae32d4765f5fe924386e4297479f8bdbf
Kleber Garcia Alcatrao <kleber@ziontechgroup.com>
2025-06-25 19:51:21 -0700

    fix: enable navigation from checkout

--- pages/checkout/[id].tsx
+++ pages/checkout/[id].tsx
@@ -1,11 +1,1 @@
-import React, { lazy, Suspense } from 'react';
-
-const CheckoutPage = lazy(() => import('@/pages/CheckoutPage'));
-
-export default function CheckoutId() {
-  return (
-    <Suspense fallback={<div>Loading checkout...</div>}>
-      <CheckoutPage />
-    </Suspense>
-  );
-}
+export { default } from '@/pages/CheckoutPage';
