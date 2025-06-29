2f478a796767cc90e65b0257fd50bc6c875ed7ed
Kleber Garcia Alcatrao <kleber@ziontechgroup.com>
2025-06-29 01:31:00 -0300

    feat(ui): auto-hide init spinner

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,10 +3,12 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 class MyDocument extends Document {
   render() {
     const themeScript = `(() => {try{var t=localStorage.getItem('theme');var e=t==='dark'||(t==='system'?!window.matchMedia||(window.matchMedia('(prefers-color-scheme: dark)').matches):false);var n=e?'dark':'light';var r=document.documentElement;r.classList.add(n);r.setAttribute('data-theme',n);}catch(e){}})();`;
+    const loaderTimeoutScript = `setTimeout(function(){var el=document.getElementById('initial-loader');if(el){el.style.display='none';}},10000);`;
     return (
       <Html>
         <Head>
           <script dangerouslySetInnerHTML={{ __html: themeScript }} />
+          <script dangerouslySetInnerHTML={{ __html: loaderTimeoutScript }} />
         </Head>
         <body>
           <Main />
