35c89c0d561653d32106f15d50f68b45fce71d21
Kleber Garcia Alcatrao <kleber@ziontechgroup.com>
2025-07-05 22:40:51 -0300

    fix: resolve React.Children.only error in LanguageSelector by removing problematic visual indicator

--- src/components/header/LanguageSelector.tsx
+++ src/components/header/LanguageSelector.tsx
@@ -63,8 +63,6 @@ export function LanguageSelector() {
             {currentFlag}
           </span>
           <span className="sr-only">{t('general.select_language')}</span>
-          {/* Visual indicator that this is interactive */}
-          <div className="absolute -bottom-0.5 left-1/2 transform -translate-x-1/2 w-1 h-1 rounded-full bg-current opacity-50" />
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent 
