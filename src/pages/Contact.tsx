b7270e9e814e7724e3ae2401be7ea791872b1da8
Kleber Garcia Alcatrao <kleber@ziontechgroup.com>
2025-07-10 09:12:18 -0300

    fix(lint): remove unused import 'Header' in Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import { Header } from '@/components/Header';
 import { SEO } from '@/components/SEO';
 import { GradientHeading } from '@/components/GradientHeading';
 import { Button } from '@/components/ui/button';
