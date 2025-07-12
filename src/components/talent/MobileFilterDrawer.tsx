1a625f81bbf3774dd7bcefe43fe1c41db69ee6b0
Kleber Garcia Alcatrao <kleber@ziontechgroup.com>
2025-07-12 20:16:39 -0300

    Refactor MobileFilterDrawer: use FilterSidebarProps instead of any for filterProps

--- src/components/talent/MobileFilterDrawer.tsx
+++ src/components/talent/MobileFilterDrawer.tsx
@@ -2,11 +2,12 @@
 import React from "react";
 import { Drawer } from "@/components/ui/drawer";
 import { FilterSidebar } from "@/components/talent/FilterSidebar";
+import type { FilterSidebarProps } from "@/types/filters";
 
 interface MobileFilterDrawerProps {
   isMobileFilterOpen: boolean;
   setIsMobileFilterOpen: (isOpen: boolean) => void;
-  filterProps: any; // We'll pass all filter-related props through this object
+  filterProps: FilterSidebarProps;
 }
 
 export function MobileFilterDrawer({ 
