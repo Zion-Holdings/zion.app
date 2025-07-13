36b40c93746b13d3204a5ea25606bda1434f3e46
Kleber Garcia Alcatrao <kleber@ziontechgroup.com>
2025-07-13 11:30:32 -0300

    Comment out disallowed console.log in temp_check_env.cjs to resolve linter warning

--- temp_check_env.cjs
+++ temp_check_env.cjs
@@ -5,7 +5,7 @@
 // Simplified logger functions - using proper structured logging
 function logInfo(message, ...args) {
   // eslint-disable-next-line no-console
-  console.log(`[INFO] ${message}`, ...args);
+  // console.log(`[INFO] ${message}`, ...args);
 }
 
 function logWarn(message, ...args) {
