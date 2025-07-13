ccce6757ad2c877fd650d792554659ca95df510a
Kleber Garcia Alcatrao <kleber@ziontechgroup.com>
2025-07-12 21:01:30 -0300

    chore: remove unused import 'Separator' from ChatBotPanel to resolve linter warning.

--- src/components/support/ChatBotPanel.tsx
+++ src/components/support/ChatBotPanel.tsx
@@ -4,7 +4,6 @@ import { logDebug, logErrorToProduction } from '@/utils/productionLogger';
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import { Separator } from "@/components/ui/separator";
 import { toast } from "@/components/ui/use-toast";
 import { cn } from "@/lib/utils";
 import { ChatMessage } from "./ChatMessage";
