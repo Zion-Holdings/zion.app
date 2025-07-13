3af583cdc49cae977a614a0208c06d289de544d2
Kleber Garcia Alcatrao <kleber@ziontechgroup.com>
2025-07-13 00:51:34 -0300

    refactor: remove unused fieldState and formState from BudgetFields render props to resolve linter/type errors

--- src/components/profile/hire-request/BudgetFields.tsx
+++ src/components/profile/hire-request/BudgetFields.tsx
@@ -17,7 +17,7 @@ export function BudgetFields({ form, _talent }: BudgetFieldsProps) {
       <FormField
         control={form.control}
         name="budgetMin"
-        render={({ field, fieldState, formState }) => (
+        render={({ field }) => (
           <FormItem>
             <FormLabel className="text-white">Budget (Min)</FormLabel>
             <FormControl>
@@ -38,7 +38,7 @@ export function BudgetFields({ form, _talent }: BudgetFieldsProps) {
       <FormField
         control={form.control}
         name="budgetMax"
-        render={({ field, fieldState, formState }) => (
+        render={({ field }) => (
           <FormItem>
             <FormLabel className="text-white">Budget (Max)</FormLabel>
             <FormControl>
