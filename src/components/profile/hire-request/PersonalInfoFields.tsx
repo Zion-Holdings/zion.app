a58143bc484ca44268356b83599feaf04eb6888f
Kleber Garcia Alcatrao <kleber@ziontechgroup.com>
2025-07-13 01:23:54 -0300

    fix: destructure only 'field' in render props in PersonalInfoFields to resolve linter/type errors

--- src/components/profile/hire-request/PersonalInfoFields.tsx
+++ src/components/profile/hire-request/PersonalInfoFields.tsx
@@ -14,7 +14,7 @@ export function PersonalInfoFields({ form }: PersonalInfoFieldsProps) {
       <FormField
         control={form.control}
         name="requesterName"
-        render={({ field, fieldState, formState }) => (
+        render={({ field }) => (
           <FormItem>
             <FormLabel className="text-white">Your Name</FormLabel>
             <FormControl>
@@ -32,7 +32,7 @@ export function PersonalInfoFields({ form }: PersonalInfoFieldsProps) {
       <FormField
         control={form.control}
         name="requesterEmail"
-        render={({ field, fieldState, formState }) => (
+        render={({ field }) => (
           <FormItem>
             <FormLabel className="text-white">Your Email</FormLabel>
             <FormControl>
