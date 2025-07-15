da3c07fc2a3e26c8624a0d3e77587771777fa821
Kleber Garcia Alcatrao <kleber@ziontechgroup.com>
2025-07-15 00:56:29 -0300

    Fix import paths for Careers and TimeOS pages to resolve Next.js build errors

--- pages/careers.tsx
+++ pages/careers.tsx
@@ -1,1 +1,1 @@
-export { default } from '@/src/pages/Careers';
+export { default } from '../src/pages/Careers';
--- pages/timeos.tsx
+++ pages/timeos.tsx
@@ -1,1 +1,1 @@
-export { default } from '@/pages/TimeOS';
+export { default } from '../src/pages/TimeOS';
