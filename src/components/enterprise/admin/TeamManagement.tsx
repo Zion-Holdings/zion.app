595d7a48fd175c85bc50c7b1618ac4ef459e4b1d
Kleber Garcia Alcatrao <kleber@ziontechgroup.com>
2025-07-17 20:21:18 -0300

    fix: import Trash from centralized icons export in TeamManagement

--- src/components/enterprise/admin/TeamManagement.tsx
+++ src/components/enterprise/admin/TeamManagement.tsx
@@ -1,6 +1,6 @@
 
 import React, { useState } from "react";
-import { Mail, UserPlus } from '@/components/ui/icons';
+import { Mail, UserPlus, Trash } from '@/components/ui/icons';
 
 import {
   Table,
