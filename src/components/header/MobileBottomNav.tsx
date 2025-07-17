261a56a050af983fc6bb38150703818f91dc9ee0
Kleber Garcia Alcatrao <kleber@ziontechgroup.com>
2025-07-17 20:58:01 -0300

    fix: import MessageCircle and ShoppingCart from centralized icons export in MobileBottomNav

--- src/components/header/MobileBottomNav.tsx
+++ src/components/header/MobileBottomNav.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Home, Search, Heart, MessageSquare, User } from '@/components/ui/icons';
+import { Home, Search, Heart, MessageSquare, User, MessageCircle, ShoppingCart } from '@/components/ui/icons';
 import { useRouter } from "next/router";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
