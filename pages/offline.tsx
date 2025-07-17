e2cc8610cb702bf9fe58e3b3457ec4342d09660c
Kleber Garcia Alcatrao <kleber@ziontechgroup.com>
2025-07-17 19:08:52 -0300

    fix: import ShoppingCart from centralized icons export in offline page

--- pages/offline.tsx
+++ pages/offline.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 import { motion } from 'framer-motion'
-import { WifiOff, RefreshCw, Home, Clock, Bookmark, Search } from '@/components/ui/icons';
+import { WifiOff, RefreshCw, Home, Clock, Bookmark, Search, ShoppingCart } from '@/components/ui/icons';
 
 
 
