0f89033c2fd44ce39e8025e8aaae65326383e04f
Kleber Garcia Alcatrao <kleber@ziontechgroup.com>
2025-07-17 23:59:37 -0300

    refactor: standardize getStaticProps and getStaticPaths naming conventions
    - Updated getStaticProps and getStaticPaths functions in categories and case-studies pages by removing underscore prefixes for improved consistency and readability.
    - Enhanced error handling in getStaticProps to include error parameters for better logging clarity.

--- pages/community/blog.tsx
+++ pages/community/blog.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import type { GetServerSideProps } from 'next';
 import Blog from '../../src/pages/Blog';
 
-// export const _getServerSideProps: GetServerSideProps = async () => {
+export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {}
   };
