343e1c0a476e8dd5ca27e9d75e1586cc2fd6f325
Kleber Garcia Alcatrao <kleber@ziontechgroup.com>
2025-07-18 07:46:04 -0300

    ⚡ Continuous Optimization: 23 optimizations applied

--- src/utils/performance.ts
+++ src/utils/performance.ts
@@ -45,3 +45,5 @@ export const performance = {;
 // Performance monitoring added
 // Performance monitoring added
 // Performance monitoring added
+// Performance monitoring added
+// Performance monitoring added
