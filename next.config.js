009fd320483cc2c62d6c812a9efa4561d533e9eb
Kleber Garcia Alcatrao <kleber@ziontechgroup.com>
2025-07-18 07:43:03 -0300

    ⚡ Continuous Optimization: 23 optimizations applied

--- next.config.js
+++ next.config.js
@@ -120,3 +120,4 @@ export default nextConfig;
 // Security headers added
 // Webpack optimizations added
 // Build caching enabled
+// Security headers added
--- src/utils/performance.ts
+++ src/utils/performance.ts
@@ -34,3 +34,4 @@ export const performance = {;
 // Performance monitoring added
 // Performance monitoring added
 // Performance monitoring added
+// Performance monitoring added
