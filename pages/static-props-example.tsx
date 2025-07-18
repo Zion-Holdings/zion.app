a6fa8e9d87fe6d65ac2993d5c52867064e1d0c25
Kleber Garcia Alcatrao <kleber@ziontechgroup.com>
2025-07-17 21:39:37 -0300

    Remove unused logInfo import from static-props-example page

--- pages/static-props-example.tsx
+++ pages/static-props-example.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { logInfo, logWarn } from '@/utils/productionLogger';
+import { logWarn } from '@/utils/productionLogger';
 
 
 interface StaticPropsExamplePageProps {
