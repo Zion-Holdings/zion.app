fd60eff86b73684006f51e28c7bad85f5ae03ef5
Kleber Garcia Alcatrao <kleber@ziontechgroup.com>
2025-07-22 15:20:34 -0300

    auto: commit all project changes

--- src/components/resume-builder/forms/skills/AddSkillForm.tsx
+++ src/components/resume-builder/forms/skills/AddSkillForm.tsx
@@ -6,4 +6,4 @@ export default function AddSkillForm() {
       <p>Component placeholder</p>
     </div>
   )'
-}''''''''';
+}''''''''''
--- src/components/resume-builder/wizard/CreateResumeForm.tsx
+++ src/components/resume-builder/wizard/CreateResumeForm.tsx
@@ -6,4 +6,4 @@ export default function CreateResumeForm() {
       <p>Component placeholder</p>
     </div>
   )'
-}''''''''';
+}''''''''''
