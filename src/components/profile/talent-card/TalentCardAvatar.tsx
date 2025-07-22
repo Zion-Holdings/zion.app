f8cae60f043fc3a09705065c6f5bfc90c37fde6c
Kleber Garcia Alcatrao <kleber@ziontechgroup.com>
2025-07-22 16:09:29 -0300

    auto: commit all project changes

--- src/components/profile/talent-card/TalentCardAvatar.tsx
+++ src/components/profile/talent-card/TalentCardAvatar.tsx
@@ -6,4 +6,4 @@ export default function TalentCardAvatar() {
       <p>Component placeholder</p>
     </div>
   )'
-}''''''''''';
+}''''''''''''
--- src/components/projects/milestones/ProjectMilestonesContent.tsx
+++ src/components/projects/milestones/ProjectMilestonesContent.tsx
@@ -6,4 +6,4 @@ export default function ProjectMilestonesContent() {
       <p>Component placeholder</p>
     </div>
   )'
-}''''''''''';
+}''''''''''''
