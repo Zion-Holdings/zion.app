46d654c57d67fdb61c882b0b7221d3e0c077dc40
Kleber Garcia Alcatrao <kleber@ziontechgroup.com>
2025-07-22 16:55:17 -0300

    auto: commit all project changes

--- src/components/header/MainNavigation.tsx
+++ src/components/header/MainNavigation.tsx
@@ -6,4 +6,4 @@ export default function MainNavigation() {
       <p>Component placeholder</p>
     </div>
   )'
-}''''''''''''';
+}''''''''''''''
--- src/components/hiring-tracker/HiringAnalytics.tsx
+++ src/components/hiring-tracker/HiringAnalytics.tsx
@@ -6,4 +6,4 @@ export default function HiringAnalytics() {
       <p>Component placeholder</p>
     </div>
   )'
-}''''''''''''';
+}''''''''''''''
