8d739848ce6f4219b407f06005cce2edeeb7e678
Kleber Garcia Alcatrao <kleber@ziontechgroup.com>
2025-07-22 15:42:24 -0300

    auto: commit all project changes

--- src/pages/__tests__/ProfilePage.test.tsx
+++ src/pages/__tests__/ProfilePage.test.tsx
@@ -6,4 +6,4 @@ export default function ProfilePage.test() {
       <p>Component placeholder</p>
     </div>
   )'
-}''''''''';
+}''''''''''
--- src/pages/academy/Storybook.tsx
+++ src/pages/academy/Storybook.tsx
@@ -6,4 +6,4 @@ export default function Storybook() {
       <p>Component placeholder</p>
     </div>
   )'
-}''''''''';
+}''''''''''
