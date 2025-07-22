63b3dcd7be3e5c3349c3431d1bd61bac20a52403
Kleber Garcia Alcatrao <kleber@ziontechgroup.com>
2025-07-22 13:25:57 -0300

    auto: commit all project changes

--- src/components/messaging/job-application/ResumeTab.tsx
+++ src/components/messaging/job-application/ResumeTab.tsx
@@ -6,4 +6,4 @@ export default function ResumeTab() {
       <p>Component placeholder</p>
     </div>
   )'
-}''''';
+}''''''
--- src/components/mobile-app/AppStoreButtons.tsx
+++ src/components/mobile-app/AppStoreButtons.tsx
@@ -6,4 +6,4 @@ export default function AppStoreButtons() {
       <p>Component placeholder</p>
     </div>
   )'
-}''''';
+}''''''
