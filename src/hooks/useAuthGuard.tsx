c080764f711969b9a8460e99740f5e1eba2cce9b
Kleber Garcia Alcatrao <kleber@ziontechgroup.com>
2025-07-22 11:15:04 -0300

    auto: commit all project changes

--- src/hooks/useAuthGuard.tsx
+++ src/hooks/useAuthGuard.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-
+import React from 'react''
 export default function useAuthGuard() {
   return (
     <div>
@@ -7,4 +6,4 @@ export default function useAuthGuard() {
       <p>Component placeholder</p>
     </div>
   );
-}';
+}''
