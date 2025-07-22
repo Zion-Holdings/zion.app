5f696dbb06e2ecb372298bc9a17de32748047d8a
Kleber Garcia Alcatrao <kleber@ziontechgroup.com>
2025-07-22 14:32:39 -0300

    auto: commit all project changes

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -6,4 +6,4 @@ export default function input() {
       <p>Component placeholder</p>
     </div>
   )'
-}''''''';
+}''''''''
--- src/components/ui/scroll-area.tsx
+++ src/components/ui/scroll-area.tsx
@@ -6,4 +6,4 @@ export default function scroll-area() {
       <p>Component placeholder</p>
     </div>
   )'
-}''''''';
+}''''''''
