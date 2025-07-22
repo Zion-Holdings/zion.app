59cd4570d76c964ab14cee21997adf95156b15bc
Kleber Garcia Alcatrao <kleber@ziontechgroup.com>
2025-07-22 16:51:07 -0300

    auto: commit all project changes

--- src/components/auth/login/LoginForm.tsx
+++ src/components/auth/login/LoginForm.tsx
@@ -6,4 +6,4 @@ export default function LoginForm() {
       <p>Component placeholder</p>
     </div>
   )'
-}''''''''''''';
+}''''''''''''''
--- src/components/cart/CartDrawer.tsx
+++ src/components/cart/CartDrawer.tsx
@@ -6,4 +6,4 @@ export default function CartDrawer() {
       <p>Component placeholder</p>
     </div>
   )'
-}''''''''''''';
+}''''''''''''''
