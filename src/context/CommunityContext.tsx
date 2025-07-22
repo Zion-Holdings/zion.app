01e9369165eece60e7392b40e1b771ea8ff98f8a
Kleber Garcia Alcatrao <kleber@ziontechgroup.com>
2025-07-22 16:18:51 -0300

    auto: commit all project changes

--- src/context/CommunityContext.tsx
+++ src/context/CommunityContext.tsx
@@ -6,4 +6,4 @@ export default function CommunityContext() {
       <p>Component placeholder</p>
     </div>
   )'
-}''''''''''';
+}''''''''''''
--- src/context/GlobalLoaderContext.tsx
+++ src/context/GlobalLoaderContext.tsx
@@ -6,4 +6,4 @@ export default function GlobalLoaderContext() {
       <p>Component placeholder</p>
     </div>
   )'
-}''''''''''';
+}''''''''''''
