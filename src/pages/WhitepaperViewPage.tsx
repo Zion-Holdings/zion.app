5e15629a22300fd9454e3eb9d155d96e3ca17da5
Kleber Garcia Alcatrao <kleber@ziontechgroup.com>
2025-07-22 17:25:27 -0300

    auto: commit all project changes

--- src/pages/WhitepaperViewPage.tsx
+++ src/pages/WhitepaperViewPage.tsx
@@ -6,4 +6,4 @@ export default function WhitepaperViewPage() {
       <p>Component placeholder</p>
     </div>
   )'
-}''''''''''''';
+}''''''''''''''
--- src/pages/ZionBook.tsx
+++ src/pages/ZionBook.tsx
@@ -6,4 +6,4 @@ export default function ZionBook() {
       <p>Component placeholder</p>
     </div>
   )'
-}''''''''''''';
+}''''''''''''''
