2a30f82465bb8e4f8009a70a236ae89b6afa904a
Zion Tech Group <kleber@ziontechgroup.com>
2026-07-05 02:12:38 -0300

    feat: new auxiliary services, blog SEO bundle, sitemap refresh, outreach script cleanup

--- app/data/servicesData.ts
+++ app/data/servicesData.ts
@@ -119,6 +119,7 @@ import { wave293ItServices, wave293AiServices, wave293SecurityServices, wave293C
 import { wave294ItServices, wave294AiServices, wave294SecurityServices, wave294CloudServices, wave294DataServices, wave294AutomationServices, wave294MicroSaasServices } from './wave294';
 import { wave295ItServices, wave295AiServices, wave295SecurityServices, wave295CloudServices, wave295DataServices, wave295AutomationServices, wave295MicroSaasServices } from './wave295';
 import { wave296ItServices, wave296AiServices, wave296SecurityServices, wave296CloudServices, wave296DataServices, wave296AutomationServices, wave296MicroSaasServices } from './wave296';
+import { waveAuxServices } from './waveAuxServices';
 
 export const aiServices: Service[] = [
   {
@@ -1449,4 +1450,5 @@ export const allServices: Service[] = [
   ...microSaasServices,
   ...devOpsServices,
   ...emailIntelligenceServices,
+  ...waveAuxServices,
 ];
