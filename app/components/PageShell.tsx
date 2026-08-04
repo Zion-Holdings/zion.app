d9494e7ae445084e9ec44201aa13c5d29db4dc15
Kleber Garcia <kleber@ziontechgroup.com>
2026-08-03 21:03:51 -0300

    fix: add missing PageShell and JsonLd components for generated blog pages

---
+++ app/components/PageShell.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+export default function PageShell({ title, description, canonical, jsonLd, children }) {
+  return (
+    <>
+      {jsonLd && (
+        <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }} />
+      )}
+      {children}
+    </>
+  );
+}
