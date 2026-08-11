74502d2a615ad1479df9e238f0b2f0e973db6cc5
Kleber Garcia <kleber@ziontechgroup.com>
2026-08-11 12:38:31 -0300

    fix: ensure JSON-LD script tags closed in blog pages

--- app/blog/apm-application-performance-common-mistakes/page.tsx
+++ app/blog/apm-application-performance-common-mistakes/page.tsx
@@ -75,8 +75,9 @@ Leading indicators for apm application performance: common mistakes: workflow co
 
         <script
           type="application/ld+json"
-          dangerouslySetInnerHTML={__html: JSON.stringify(jsonLd)}
-        />
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        >
+        </script>
       </article>
     </div>
   );
